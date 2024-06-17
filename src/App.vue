8137c3748dce7c63c151224eef25b1f366dd950f
leonmccarthy <leonmccarthy05@gmail.com>
2024-06-17 18:31:34 +0300

    Added search input component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
+<script setup>
+  import SearchInput from './components/SearchInput.vue';
+</script>
 <template>
   <main>
-    Home
+
+    <!-- Date -->
+    <div class="text-center mb-6">
+      {{ new Date().toLocaleDateString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      }) }}
+    </div>
+
+    <!-- Search -->
+     <div>
+        <SearchInput />
+     </div>
   </main>
 </template>
